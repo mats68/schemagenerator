f60c045e37fd8978f9024a7cbfa5a44d4a748141
M. Thaler <mats68@gmail.com>
2020-10-03 09:07:55 +0200

    sidenav

--- src/app/components/mt-sidenav/mt-sidenav.component.ts
+++ src/app/components/mt-sidenav/mt-sidenav.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit } from '@angular/core';
 import { MtBaseComponent } from 'src/app/base/mt-base/mt-base.component';
-
+import { MediaMatcher } from '@angular/cdk/layout';
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'mt-sidenav',
   templateUrl: './mt-sidenav.component.html',
   styleUrls: ['./mt-sidenav.component.scss']
 })
-export class MtSidenavComponent extends MtBaseComponent implements OnInit {
+export class MtSidenavComponent extends MtBaseComponent implements OnInit, OnDestroy, AfterViewInit {
+  private _mobileQueryListener: () => void;
+  mobileQuery: MediaQueryList;
+  showSpinner: boolean;
+  userName: string;
+  isAdmin: boolean;
 
+  private autoLogoutSubscription: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private media: MediaMatcher,
+  ) {
+    super();
+    this.mobileQuery = this.media.matchMedia('(max-width: 1000px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    // tslint:disable-next-line: deprecation
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+    this.autoLogoutSubscription.unsubscribe();
+  }
+
+  ngAfterViewInit(): void {
+    this.changeDetectorRef.detectChanges();
+  }
 }
